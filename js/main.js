b0e4b4a9dff35d4070bb1b7b665dd3f9b811fbaa
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-10-23 15:08:38 +0300

    Отображает фотографии других пользователей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import {createPhotos} from './photo-description.js';
 const PHOTO_DESCRIPTION_COUNT = 25;
 createPhotos(PHOTO_DESCRIPTION_COUNT);
+import {pictureFragment} from './draws-pictures.js';
+pictureFragment();
