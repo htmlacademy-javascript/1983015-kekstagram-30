911cf8f108776289fd0361b19fda61cb4e339ac0
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-10-24 14:46:02 +0300

    добавляет модуль

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { createPhotos } from './photo-description.js';
-const PHOTO_DESCRIPTION_COUNT = 25;
-createPhotos(PHOTO_DESCRIPTION_COUNT);
-import './draws-pictures.js';
+import {renderPictures } from './render-pictures.js';
 
+renderPictures();
