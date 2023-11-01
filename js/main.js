b35da48734d57ed2416d500a2ceda3053367e779
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-11-01 10:44:30 +0300

    Убирает импорт

--- js/main.js
+++ js/main.js
@@ -3,6 +3,3 @@ import {createPhotos, PHOTO_DESCRIPTION_COUNT} from './create-photos.js';
 import { renderPictures } from './render-pictures.js';
 renderPictures(createPhotos(PHOTO_DESCRIPTION_COUNT));
 
-
-import './overlay-big-picture.js';
-
