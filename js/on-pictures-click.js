ea698aaadc0edc8707ffd79b7dee1355cbf2f9d2
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-11-01 10:45:55 +0300

    Переменовывает модуль

--- js/on-pictures-click.js
+++ js/on-pictures-click.js
@@ -1,11 +1,8 @@
-import { isEscapeKey } from './util';
-import { picturesContainer } from './render-pictures';
+import { isEscapeKey } from './util.js';
 
 const bodyContainer = document.querySelector('body');
 const overlayBigPicture = document.querySelector('.big-picture');
 const overlayBigPictureClose = document.querySelector('.big-picture__cancel');
-const countComment = overlayBigPicture.querySelector('.social__comment-count');
-const commentLoader = overlayBigPicture.querySelector('.comments-loader');
 
 const onPictureEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -15,16 +12,13 @@ const onPictureEscKeydown = (evt) => {
 };
 
 const openBigPicture = (evt) => {
-  if (evt.target.closest('.picture').dataset.photoId) {
-    evt.preventDefault();
+  evt.preventDefault();
+  if (evt.target.closest('.picture')) {
     overlayBigPicture.classList.remove('hidden');
     bodyContainer.classList.add('modal-open');
-    countComment.classList.add('hidden');
-    commentLoader.classList.add('hidden');
     document.addEventListener('keydown', onPictureEscKeydown);
   }
 };
-
 
 const closeBigPicture = () => {
   overlayBigPicture.classList.add('hidden');
@@ -32,9 +26,7 @@ const closeBigPicture = () => {
   document.removeEventListener('keydown', onPictureEscKeydown);
 };
 
-picturesContainer.addEventListener('click', openBigPicture);
-
 overlayBigPictureClose.addEventListener('click', closeBigPicture);
 
-export { openBigPicture };
+export { openBigPicture, overlayBigPicture };
 
