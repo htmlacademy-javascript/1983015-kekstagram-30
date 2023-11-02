4fed85e9ad299037abd256dca0abe30f0dc4ffdd
Keks <keks@htmlacademy.ru>
2023-11-02 15:31:33 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,5 @@
+import {createPhotos, PHOTO_DESCRIPTION_COUNT} from './create-photos.js';
+
+import { renderPictures } from './render-pictures.js';
+renderPictures(createPhotos(PHOTO_DESCRIPTION_COUNT));
+
---
+++ 8/js/render-pictures.js
@@ -0,0 +1,39 @@
+import { renderBigPicture } from './render-overlay-picture.js';
+import { openBigPicture} from './on-pictures-click.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const picturesTitle = document.querySelector('.pictures__title');
+picturesTitle.classList.remove('visually-hidden');
+const pictureTemplate = document.querySelector('#picture')
+  .content.querySelector('.picture');
+
+const renderPicture = ({ url, description, comments, likes, id }) => {
+  const thumbnail = pictureTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.dataset.photoId = id;
+
+  return thumbnail;
+};
+
+const renderPictures = (photoArray) => {
+  const pictureFragment = document.createDocumentFragment();
+
+  photoArray.forEach((photoItem) => {
+    const thumbnails = renderPicture(photoItem);
+    pictureFragment.append(thumbnails);
+
+    const onThumbnailClick = () => {
+      renderBigPicture(photoItem);
+      picturesContainer.addEventListener('click', openBigPicture);
+    };
+    thumbnails.addEventListener('click', onThumbnailClick);
+  });
+
+  picturesContainer.append(pictureFragment);
+};
+
+export {renderPictures};
+
