4486eeb3141bfe7a2e150d5f6545846a438b2c8a
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-11-04 15:22:27 +0300

    Добаыляет ID

--- js/render-pictures.js
+++ js/render-pictures.js
@@ -1,38 +1,28 @@
-import { renderOverlayPicture } from './render-overlay-picture.js';
-import { openBigPicture} from './on-pictures-click.js';
-
-const picturesContainer = document.querySelector('.pictures');
 const picturesTitle = document.querySelector('.pictures__title');
 picturesTitle.classList.remove('visually-hidden');
 const pictureTemplate = document.querySelector('#picture')
   .content.querySelector('.picture');
 
-const renderPicture = ({ url, description, comments, likes}) => {
+const renderPicture = ({ url, description, comments, likes, id}) => {
   const thumbnail = pictureTemplate.cloneNode(true);
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.dataset.photoId = id;
 
   return thumbnail;
 };
 
-const renderPictures = (photoArray) => {
+const renderPictures = (photos, containerThumbnails) => {
   const pictureFragment = document.createDocumentFragment();
 
-  photoArray.forEach((photoItem) => {
+  photos.forEach((photoItem) => {
     const thumbnails = renderPicture(photoItem);
     pictureFragment.append(thumbnails);
-
-    const onThumbnailClick = () => {
-      renderOverlayPicture(photoItem);
-      picturesContainer.addEventListener('click', openBigPicture);
-    };
-
-    thumbnails.addEventListener('click', onThumbnailClick);
   });
 
-  picturesContainer.append(pictureFragment);
+  containerThumbnails.append(pictureFragment);
 };
 
 export {renderPictures};
