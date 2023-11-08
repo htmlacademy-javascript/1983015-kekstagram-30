fd130591a258ae9b63fafbebb613709dad7b56ca
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-11-08 11:30:52 +0300

    Дорабатывает валидацию

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -1,14 +1,23 @@
 import { isEscapeKey } from './util.js';
 import { bodyContainer } from './overlay-pictures.js';
 
-const imgUploadForm = document.querySelector('.img-upload__form');
+const NUMBER_OF_HASHTAGS = 5;
+const REXEXP_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
 
 //const imgUploadSubmitButton = imgUploadForm.querySelector('.img-upload__submit');
 //imgUploadSubmitButton.disabled = true;
-
+const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
 const imgUploadCancelButton = imgUploadForm.querySelector('.img-upload__cancel');
 const imgUploadInput = imgUploadForm.querySelector('.img-upload__input ');
+const textHashtags = imgUploadForm.querySelector('.text__hashtags');
+const textComments = imgUploadForm.querySelector('.text__description');
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error'
+});
 
 const showEditingForm = () => {
   imgUploadOverlay.classList.remove('hidden');
@@ -16,31 +25,76 @@ const showEditingForm = () => {
   document.addEventListener('keydown', onImgEscKeydown);
 };
 
-const onShowFormButtonClick = () => {
-  showEditingForm();
-};
-
 const closeEditingForm = () => {
+  imgUploadForm.reset();
+  pristine.reset();
   imgUploadOverlay.classList.add('hidden');
   bodyContainer.classList.remove('modal-open');
   document.removeEventListener('keydown', onImgEscKeydown);
-  imgUploadInput.value = '';
 };
+
+const isFieldFocused = () => document.activeElement === textHashtags || document.activeElement === textComments;
+
+function onImgEscKeydown(evt) {
+  if (isEscapeKey(evt) && isFieldFocused()) {
+    evt.preventDefault();
+    closeEditingForm();
+  }
+}
+
+const normalizeHashtags = (str) => str.trim().split(' ').filter((hashtag) => Boolean(hashtag.length));
+
+const checkValidateHashtag = (value) => normalizeHashtags(value).every((hashtag) => REXEXP_HASHTAG.test(hashtag));
+
+const checkHashtagListLength = (value) => normalizeHashtags(value).length <= NUMBER_OF_HASHTAGS;
+
+const checkUniqueHashtags = (value) => {
+  const loserCaseHashtags = normalizeHashtags(value).map((hashtag) => hashtag.toLowerCase());
+  return loserCaseHashtags.length === new Set(loserCaseHashtags).size;
+};
+
+//введён невалидный хэш-тег
+pristine.addValidator(
+  textHashtags,
+  checkValidateHashtag,
+  'Введён невалидный хэш-тег',
+  1,
+  true
+);
+
+//превышено количество хэш-тегов
+pristine.addValidator(
+  textHashtags,
+  checkHashtagListLength,
+  'Превышено количество хэш-тегов',
+  2,
+  true
+);
+
+//хэш-теги повторяются
+pristine.addValidator(
+  textHashtags,
+  checkUniqueHashtags,
+  'Хэш-теги повторяются',
+  3,
+  true
+);
 
 const onCloseFormButtonClick = () => {
   closeEditingForm();
 };
 
-function onImgEscKeydown(evt) {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    //evt.stopPropagation();
-    closeEditingForm();
-  }
-}
+const onShowFormInputChange = () => {
+  showEditingForm();
+};
 
-imgUploadInput.addEventListener('change', onShowFormButtonClick);
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+imgUploadForm.addEventListener('submit', onFormSubmit);
+
+imgUploadInput.addEventListener('change', onShowFormInputChange);
 
 imgUploadCancelButton.addEventListener('click', onCloseFormButtonClick);
-
-export {imgUploadForm};
