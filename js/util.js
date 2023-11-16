e7fa58fbaec20b42841c1a841d833bacea483552
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-11-16 10:06:43 +0300

    Добавляет вспомогател ф-ции для фильтрации и устранения дребезга

--- js/util.js
+++ js/util.js
@@ -13,8 +13,30 @@ const showDataError = () => {
   }, DATA_ERROR_SHOW_TIME);
 };
 
+const shufflePictures = (photos) => {
+  for (let i = photos.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [photos[i], photos[j]] = [photos[j], photos[i]];
+  }
+};
+
+const sortPictures = (photos) => {
+  photos.sort((a, b) => b.comments.length - a.comments.length);
+};
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 export {
   isEscapeKey,
-  showDataError
+  showDataError,
+  shufflePictures,
+  sortPictures,
+  debounce
 };
 
