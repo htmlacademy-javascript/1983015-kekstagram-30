7d4e3135a25c35b81f8365332f725698dd481543
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-11-17 14:33:52 +0300

    Подключает фотографии

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ import './img-upload-form.js';
 import { getPicture } from './api.js';
 import { showDataError } from './util.js';
 import { onFormSubmit, closeEditingForm } from './img-upload-form.js';
+import {initFilters} from './filters.js';
 
 getPicture()
   .then((pictures) => {
     renderGallery(pictures);
+    initFilters(pictures);
   })
   .catch(
     (err) => {
